60dc61fa0c6412cfbcd8e6e9abca6b1022e87f0d
kodoKingu19 <110048579+kodoKingu19@users.noreply.github.com>
2022-10-03 19:40:39 +0000

    Mon, Oct 3, 2022, 7:40 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 int get_cents(void);
-int calculate_quarters();
-int calculate_dimes();
-int calculate_nickels();
-int calculate_pennies();
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
 int main(void)
 {
@@ -13,19 +13,19 @@ int main(void)
     int cents = get_cents();
 
     // Calculate the number of quarters to give the customer
-    int quarters = calculate_quarters();
+    int quarters = calculate_quarters(cents);
     cents = cents - quarters * 25;
 
     // Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes();
+    int dimes = calculate_dimes(cents);
     cents = cents - dimes * 10;
 
     // Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels();
+    int nickels = calculate_nickels(cents);
     cents = cents - nickels * 5;
 
     // Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies();
+    int pennies = calculate_pennies(cents);
     cents = cents - pennies * 1;
 
     // Sum coins
@@ -46,7 +46,7 @@ int get_cents(void)
 
 }
 
-int calculate_quarters()
+int calculate_quarters(int cents)
 {
     int quarters = 0;
     while (cents >= 25)
@@ -57,7 +57,7 @@ int calculate_quarters()
     return quarters;
 }
 
-int calculate_dimes()
+int calculate_dimes(int cents)
 {
     int dimes = 0;
     while (cents >= 10)
@@ -68,7 +68,7 @@ int calculate_dimes()
     return dimes;
 }
 
-int calculate_nickels()
+int calculate_nickels(int cents)
 {
     int nickels = 0;
     while (cents >= 5)
@@ -79,7 +79,7 @@ int calculate_nickels()
     return nickels;
 }
 
-int calculate_pennies()
+int calculate_pennies(int cents)
 {
     int pennies = 0;
     while (cents >= 1)
