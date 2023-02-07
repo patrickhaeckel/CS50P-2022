ed98d91dd348a1e2e99d90ebd7adff8840c72f69
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-07 21:04:34 +0000

    Tue, Feb 7, 2023, 10:04 PM +01:00

---
+++ tip/tip.py
@@ -0,0 +1,5 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
