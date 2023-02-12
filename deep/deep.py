deddcf306fab71823d95eb47372c02d7627e0def
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-12 20:09:20 +0000

    Sun, Feb 12, 2023, 9:09 PM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,6 @@
 def main():
-    q = (input("whos god? "))
-    if q == ("42") | ("Forty Two") | ("forty-two"):
+    q = (input("whos god? ").strip)
+    if q == ("42") | ("Forty Two") |("forty-two"):
         print("Yes")
     else:
         print("No")
