aff17caad5f4bc24dda5de2718cd0570bdf85818
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-12 20:55:34 +0000

    Sun, Feb 12, 2023, 9:55 PM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,9 +1,9 @@
 def main():
     q = input("whos god? ").replace("-", " ").strip().upper().lower()
-    print (q)
     match q:
         case "42" | "forty two" | "FORTY TWO":
             print("Yes")
         case _:
             print("No")
 main()
+
