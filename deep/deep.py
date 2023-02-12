1fbfa888c5649fd92958d8c43b182d70c7058f79
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-12 20:46:59 +0000

    Sun, Feb 12, 2023, 9:46 PM +01:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 def main():
-    q = input("whos god? ").strip().casefold()
+    q = input("whos god? ").strip()
     match q:
         case "42" | "Forty Two" | "forty-two":
             print("Yes")
