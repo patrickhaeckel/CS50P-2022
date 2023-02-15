206e29b8e5d5e9f3442e284d5e72e001964562b0
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-15 22:00:03 +0000

    Wed, Feb 15, 2023, 11:00 PM +01:00

--- set1/extensions.py
+++ set1/extensions.py
@@ -1,7 +1,8 @@
 def main():
     name = input("name of file? ").casefold().strip()
     if (".gif") in name:
-        print("yes")
-    elif
+        print("")
+    elif ("") in name:
+        print("")
 main()
 
