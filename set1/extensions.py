145a44b5cc5eb0d6ec32d476628480392392414e
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-15 22:15:59 +0000

    Wed, Feb 15, 2023, 11:15 PM +01:00

--- set1/extensions.py
+++ set1/extensions.py
@@ -2,17 +2,17 @@ def main():
     name = input("name of file? ").casefold().replace(" ", "")
     if (".gif") in name:
         print("image/gif")
-    elif ("jpg") in name:
+    elif (".jpg") in name:
         print("image/jpg")
-    elif ("jpeg") in name:
+    elif (".jpeg") in name:
         print("image/jpeg")
-    elif ("png") in name:
-        print("image/gif")
-    elif ("pdf") in name:
+    elif (".png") in name:
+        print("image/png")
+    elif (".pdf") in name:
         print("application/pdf")
-    elif ("txt") in name:
+    elif (".txt") in name:
         print("text/plain")
-    elif ("zip") in name:
+    elif (".zip") in name:
         print("application/zip")
     else:
         print("application/octet-stream")
