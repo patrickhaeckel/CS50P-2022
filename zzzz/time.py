58387833e5ea26dbb81939102193385d107f0be4
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-16 20:56:41 +0000

    Thu, Feb 16, 2023, 9:56 PM +01:00

--- zzzz/time.py
+++ zzzz/time.py
@@ -1,11 +1,15 @@
 def main():
     time = input("what time is it? ").strip().split(":")
-    output = convert(time)
+    x = convert(time)
+    print(x)
 
 def convert(time):
     hours, minutes = time
-    
+    hours = float(hours)
+    minutes = float(minutes)
+
+
 main()
-print(output)
+
 
 
