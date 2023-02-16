3b80546f63e65e058fb7aca85f5845c47720d157
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-16 21:24:02 +0000

    Thu, Feb 16, 2023, 10:24 PM +01:00

--- set1/meal.py
+++ set1/meal.py
@@ -1,5 +1,17 @@
 def main():
-    time = input("what time is it? ").split(" ")
-    a, b, c = time
-    print(time)
+    time = input("what time is it? ").strip().split(":")
+    output = convert(time)
+    if output >= 7 and output <= 8:
+        print("breakfast time")
+    elif output >= 12 and output <= 13:
+        print("lunch time")
+    elif output >= 18 and output <= 19:
+        print("dinner time")
+
+def convert(time):
+    hours, minutes = time
+    hours = float(hours)
+    minutes = float(minutes)
+    output = (hours) + (minutes/60)
+    return output
 main()
