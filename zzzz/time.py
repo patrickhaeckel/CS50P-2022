8278feaa3961353b69115597a3b944298e69bcdb
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-16 20:50:35 +0000

    Thu, Feb 16, 2023, 9:50 PM +01:00

--- zzzz/time.py
+++ zzzz/time.py
@@ -1,20 +1,11 @@
 def main():
     time = input("what time is it? ").strip().split(":")
-    hours, minutes = time
-    hours = float(hours)
-    minutes = float(minutes)
-    output = (minutes/60) + (hours)
-    print(output)
-    if output >= 4 and output <= 5:
-        print("breakfast time")
-main()
-
-def main():
-    time = input("what time is it? ").strip().split(":")
     output = convert(time)
 
 def convert(time):
     hours, minutes = time
+    print(output)
+main()
 
 
 
