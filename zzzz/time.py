648efb4ce9b602fc3e8b215a29ba61faff97abce
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-16 20:48:47 +0000

    Thu, Feb 16, 2023, 9:48 PM +01:00

--- zzzz/time.py
+++ zzzz/time.py
@@ -9,6 +9,12 @@ def main():
         print("breakfast time")
 main()
 
-def main()
-    
+def main():
+    time = input("what time is it? ").strip().split(":")
+    output = convert(time)
+
+def convert(time):
+    hours, minutes = time
+
+
 
