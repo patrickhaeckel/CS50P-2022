367871c12d060880460634efee09bd859455a6ef
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-21 01:39:51 +0000

    Tue, Feb 21, 2023, 2:39 AM +01:00

--- set2/camel.py
+++ set2/camel.py
@@ -2,7 +2,7 @@ def main():
        v = input("name of variable ")
        for i in v:
               if i.isupper():
-                     print("_" + i.lower())
+                     print("_" + i.lower(), end="")
 
 main()
 
