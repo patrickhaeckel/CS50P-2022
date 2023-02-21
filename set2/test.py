d3fd1f0c1a8687de90a0d6258c8732b1a28470aa
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-21 01:58:08 +0000

    Tue, Feb 21, 2023, 2:58 AM +01:00

--- set2/test.py
+++ set2/test.py
@@ -4,11 +4,10 @@ def main():
     snake_case(camelCase)
 
 
-def snake_case(camelCase):
-    for letter in camelCase:
+def snake_case(n):
+    for letter in n:
         if letter.isupper():
             print("_" + letter.lower(), end="")
         else:
             print(letter, end="")
     print()
-main()
