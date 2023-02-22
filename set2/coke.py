f7f9389a78931eaa5b2f0802c05caf2eb4c6220f
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-22 23:31:21 +0000

    Thu, Feb 23, 2023, 12:31 AM +01:00

--- set2/coke.py
+++ set2/coke.py
@@ -1,10 +1,12 @@
 def main():
     list = [25, 10, 5]
     amountdue = 50
-    print(f"amount due = {amountdue}")
-    coin = int(input("Insert a coin "))
-    amountdue = 50 - coin
-    changeowed = 
+    while amountdue > 0:
+        print(f"amount due = {amountdue}")
+        coin = int(input("Insert a coin "))
+        if coin in list:
+            amountdue = amountdue - coin
+
 
 
 
