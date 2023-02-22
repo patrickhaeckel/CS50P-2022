e02d4e2ab4b41d2f10f7a528239102c384b2e4f3
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-22 23:14:15 +0000

    Thu, Feb 23, 2023, 12:14 AM +01:00

--- set2/coke.py
+++ set2/coke.py
@@ -1,8 +1,11 @@
 def main():
     list = [25, 10, 5]
     amountdue = 50
-    print(f"amount due = "{amountdue})
+    print(f"amount due = {amountdue}")
     coin = int(input("Insert a coin "))
+    amountdue = 50 - coin
+    
+
 
 
 
