0ec0eafb07925b55ffddc52dfd2c71a02c4b7283
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-23 00:19:26 +0000

    Thu, Feb 23, 2023, 1:19 AM +01:00

--- set2/coke.py
+++ set2/coke.py
@@ -1,14 +1,12 @@
 def main():
     list = [25, 10, 5]
     amountdue = 50
-    while amountdue > 0:
-        print("Amount Due: ", amountdue)
+    while amountdue >= 0:
+        print("Amount Due:", amountdue)
         coin = int(input("Insert a coin "))
         if coin in list:
             amountdue = amountdue - coin
     if amountdue < 0:
-        print("Change Owed: ", (amountdue *(-1)))
-    elif amountdue == 0:
-        print("amountdue", amountdue)
+        print("Change Owed:", (amountdue *(-1)))
 
 main()
