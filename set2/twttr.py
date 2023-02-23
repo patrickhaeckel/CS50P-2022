b4348919e5cdd586baf866644171ab4bacf3acac
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-23 00:55:57 +0000

    Thu, Feb 23, 2023, 1:55 AM +01:00

--- set2/twttr.py
+++ set2/twttr.py
@@ -1,5 +1,5 @@
 def main():
-    vocals = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
+    vocals = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     text = input("Your text here ").split()
     for text in vocals:
         print(text).replace(vocals, "")
