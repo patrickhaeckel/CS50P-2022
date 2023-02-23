dafadc414933ef7da3d004a8dc3147daa68b517f
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-23 00:02:34 +0000

    Thu, Feb 23, 2023, 1:02 AM +01:00

--- set2/coke.py
+++ set2/coke.py
@@ -6,7 +6,6 @@ def main():
         coin = int(input("Insert a coin "))
         if coin in list:
             amountdue = amountdue - coin
-            print(amountdue)
     if amountdue < 0:
         print("change owed ", (amountdue *(-1)))
 
