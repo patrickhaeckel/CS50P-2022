a014fcf8f71eb8e71a35f3d0c57f104aafb81b61
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-23 00:47:31 +0000

    Thu, Feb 23, 2023, 1:47 AM +01:00

--- set2/twttr.py
+++ set2/twttr.py
@@ -1,5 +1,8 @@
-def main()
-    vocals [a, e, i, o, u, A, E, I, O, U]
-    text = input("Your text here ").split()
-    if text in vocals:
-        text = text.split
+def main():
+    vocals = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    text = input("Your text here ").replace(vocals, "")
+    print(text)
+
+main()
+
+
