2b4801bb7c79ede0bdbbe42606f654e132b9605b
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-23 01:02:20 +0000

    Thu, Feb 23, 2023, 2:02 AM +01:00

--- set2/test.py
+++ set2/test.py
@@ -1,13 +1,3 @@
-def main():
-    camelCase = input("camelCase: ")
-    print("snake_case: ", end="")
-    snake_case(camelCase)
-
-
-def snake_case(n):
-    for letter in n:
-        if letter.isupper():
-            print("_" + letter.lower(), end="")
-        else:
-            print(letter, end="")
-    print()
+n = ("hola")
+for letter in n:
+    print(n)
