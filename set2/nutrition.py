7172ab1120996c8a79853a96cac79ea8c23becbb
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-25 23:02:37 +0000

    Sun, Feb 26, 2023, 12:02 AM +01:00

--- set2/nutrition.py
+++ set2/nutrition.py
@@ -1,4 +1,10 @@
-d = {"Apple" : 130, "Avocado" : 50, "SweetCherries" : 100}
+d = {
+    "Apple" : 130,
+    "Avocado" : 50,
+    "SweetCherries" : 100,
+    "Kiwifruit" : 90,
+    "Pear" : 100
+    }
 i = input("type your fruit here ").title().replace(" ", "")
 if i in d:
     print (d[i])
