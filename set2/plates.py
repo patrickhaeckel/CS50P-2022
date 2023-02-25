f2a68b314df5028eda4a87f501d17f5337383687
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-25 21:34:37 +0000

    Sat, Feb 25, 2023, 10:34 PM +01:00

--- set2/plates.py
+++ set2/plates.py
@@ -23,8 +23,8 @@ def is_valid(s):
         if s[i].isalpha() == False:
             if s[i] == "0":
                 return False
-        else:
-            break
+            else:
+                break
         i += 1
 
 # No periods, spaces, or punctuation marks are allowed
