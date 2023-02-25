19fee7a1b9299ef869577cb12447fce12b21f292
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-25 21:07:47 +0000

    Sat, Feb 25, 2023, 10:07 PM +01:00

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 s = input("type ")
 i = 0
 while i < len(s):
-    if s[i] == 0:
-        print("nojala")
+    if s[i].isalpha == False:
+        if s[i] == "0":
+            print("nojala")
     else:
         break
 i += 1
