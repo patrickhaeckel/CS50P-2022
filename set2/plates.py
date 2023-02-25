f86da3965012742c7e2873aeef26bc45ad785a9a
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-25 20:31:49 +0000

    Sat, Feb 25, 2023, 9:31 PM +01:00

--- set2/plates.py
+++ set2/plates.py
@@ -17,6 +17,10 @@ if len(s) < 2 or > 6:
 
 # Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would
 # be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’
+i = 0
+while s[i] == "0":
+    return false
+    
 
 
 # No periods, spaces, or punctuation marks are allowed
