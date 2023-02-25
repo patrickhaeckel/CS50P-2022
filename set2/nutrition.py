5c6bf0ea5dd22a1cc29d408501d18b2749150ab0
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-25 22:53:12 +0000

    Sat, Feb 25, 2023, 11:53 PM +01:00

--- set2/nutrition.py
+++ set2/nutrition.py
@@ -1,3 +1,3 @@
 d = {"Apple" : 130, "Avocado" : 50, "SweetCherries" : 100}
-i = input("type your fruit here ").replace(" ", "")
+i = input("type your fruit here ").replace(" ", "").casefold()
 print (d[i])
