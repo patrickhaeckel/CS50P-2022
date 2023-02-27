14fe0e004ddbf05a722ac0928626430b16dc726d
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 19:14:49 +0000

    Mon, Feb 27, 2023, 8:14 PM +01:00

--- set2/plates.py
+++ set2/plates.py
@@ -18,7 +18,7 @@ def is_valid(s):
 # Numbers cannot be used in the middle of a plate; they must come at the end.
 # For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
 # The first number used cannot be a ‘0’
-    i = 0
+    i = 2
     while (i) < len(s):
         if s[i].isalpha() == False:
             if s[i] == "0":
