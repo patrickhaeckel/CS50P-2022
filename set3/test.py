b33ed5316940c74a121db494433a3f1bf4a8d174
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 20:45:50 +0000

    Mon, Feb 27, 2023, 9:45 PM +01:00

--- set3/test.py
+++ set3/test.py
@@ -1,9 +1,5 @@
 while True:
-    try:
-        x = int(input("whats x? "))
-        print(f"x is {x}")
-    except ValueError:
-        print("x is not an integer")
+    x = int(input("type "))
 
 
 
