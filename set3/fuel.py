8b15d221b3e233ec37111d4297991d88837163bd
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 20:30:53 +0000

    Mon, Feb 27, 2023, 9:30 PM +01:00

--- set3/fuel.py
+++ set3/fuel.py
@@ -15,7 +15,7 @@ def main():
             else:
                 print(f"{z}% is in the tank")
         except (ValueError, ZeroDivisionError):
-            print(retype your division)
+            print("retype your division ")
 
 
 #  If, though, 1% or less remains, output E instead And if 99% or more remains, output F instead
