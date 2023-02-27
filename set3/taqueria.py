cc2ef1d2b87a098f51dcbd14395144009120e98b
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 22:00:40 +0000

    Mon, Feb 27, 2023, 11:00 PM +01:00

--- set3/taqueria.py
+++ set3/taqueria.py
@@ -3,11 +3,11 @@ def main():
     while True:
         try:
             d = {"Baja Taco": 4.00,    "Burrito": 7.50,    "Bowl": 8.50,   "Nachos": 11.00, "Quesadilla": 8.50, "Super Burrito": 8.50,    "Super Quesadilla": 9.50,    "Taco": 3.00,    "Tortilla Salad": 8.00}
-            o = input("Order here ")
+            o = input("Order here ").title()
             if o in d:
                 print (o[d])
-            except()
-
+        except EOFError:
+            pass
 
 main()
 
