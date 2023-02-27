7dc112a45d10e4f37f0b2a7362a5baf91e882bcc
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 19:44:46 +0000

    Mon, Feb 27, 2023, 8:44 PM +01:00

--- set3/fuel.py
+++ set3/fuel.py
@@ -1,6 +1,11 @@
 #  Program that prompts the user for a fraction, formatted as X/Y, wherein each of X and Y is an integer
-def main()
-    x = 
+def main():
+    f = input("type your fraction ").split("/")
+    x = f[0]
+    y = f[1]
+
+    print(x)
+
 
 #  And then outputs, as a percentage rounded to the nearest integer, how much fuel is in the tank
 
@@ -10,3 +15,6 @@ def main()
 
 #  If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again.
 #  (It is not necessary for Y to be 4.)
+
+
+main()
