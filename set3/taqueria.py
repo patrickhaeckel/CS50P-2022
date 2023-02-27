8105abfd3d07e5bddb044111f8bd584d33fa90b3
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 23:01:24 +0000

    Tue, Feb 28, 2023, 12:01 AM +01:00

--- set3/taqueria.py
+++ set3/taqueria.py
@@ -1,14 +1,13 @@
 #Implement a program that enables a user to place an order, prompting them for items, one per line, until the user inputs control-d. After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($) and formatted to two decimal places. Treat the user’s input case insensitively. Ignore any input that isn’t an item. Assume that every item on the menu will be titlecased.
 def main():
     Total = float(0)
-    round(Total, 2)
     while True:
         try:
             d = {"Baja Taco": 4.00,    "Burrito": 7.50,    "Bowl": 8.50,   "Nachos": 11.00, "Quesadilla": 8.50, "Super Burrito": 8.50,    "Super Quesadilla": 9.50,    "Taco": 3.00,    "Tortilla Salad": 8.00}
             o = input("Order here ").title()
             if o in d:
                 Total = ((Total + (d[o])))
-            print(f"Total: ${Total}")
+                print("Total: $", "{:.2f}".format(Total))
 
         except EOFError:
             print(end = "\n")
