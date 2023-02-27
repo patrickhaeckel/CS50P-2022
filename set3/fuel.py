c150c134f0f14fa7c55696e20af856dd3acdbe88
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 20:27:20 +0000

    Mon, Feb 27, 2023, 9:27 PM +01:00

--- set3/fuel.py
+++ set3/fuel.py
@@ -15,6 +15,7 @@ def main():
             else:
                 print(f"{z}% is in the tank")
         except ValueError:
+            ()
 
 
 #  If, though, 1% or less remains, output E instead And if 99% or more remains, output F instead
