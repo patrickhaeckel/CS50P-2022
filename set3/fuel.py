9499e4f201b68e0fb67833121ccbc031deca267e
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 20:18:05 +0000

    Mon, Feb 27, 2023, 9:18 PM +01:00

--- set3/fuel.py
+++ set3/fuel.py
@@ -1,5 +1,6 @@
 #  Program that prompts the user for a fraction, formatted as X/Y, wherein each of X and Y is an integer
 #  and then outputs, as a percentage rounded to the nearest integer, how much fuel is in the tank
+
 def main():
     f = input("type your fraction ").split("/")
     x = int(f[0])
@@ -14,6 +15,7 @@ def main():
 
 
 #  If, though, 1% or less remains, output E instead And if 99% or more remains, output F instead
+
 def tooless(n):
     if n <= 1:
         return True
@@ -23,6 +25,8 @@ def toomuch(n):
 
 #  If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again.
 #  (It is not necessary for Y to be 4.)
+
+
 
 
 main()
