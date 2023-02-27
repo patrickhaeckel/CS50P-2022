027b50114a6000192801e255bbf3c21f0acf97ed
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 20:56:18 +0000

    Mon, Feb 27, 2023, 9:56 PM +01:00

--- set3/test.py
+++ set3/test.py
@@ -1,6 +1,10 @@
-while True:
-    x = int(input("type "))
-    y = int(input("type "))
-
+def main():
+    x = int(input("type x "))
+    y = int(input("type y "))
+    if x > y:
+        main()
+    else:
+        _="."
+main()
 
 
