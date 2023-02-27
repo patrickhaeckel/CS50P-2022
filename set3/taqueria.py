90af1ec25fded54090243fe68057e38ce9c0de65
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-27 23:02:26 +0000

    Tue, Feb 28, 2023, 12:02 AM +01:00

--- set3/taqueria.py
+++ set3/taqueria.py
@@ -4,7 +4,7 @@ def main():
     while True:
         try:
             d = {"Baja Taco": 4.00,    "Burrito": 7.50,    "Bowl": 8.50,   "Nachos": 11.00, "Quesadilla": 8.50, "Super Burrito": 8.50,    "Super Quesadilla": 9.50,    "Taco": 3.00,    "Tortilla Salad": 8.00}
-            o = input("Order here ").title()
+            o = input("Item: ").title()
             if o in d:
                 Total = ((Total + (d[o])))
                 print("Total: $", "{:.2f}".format(Total))
