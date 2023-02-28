1bd0e411d983b94a4a04e61d1d1ed9b106d451f0
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-28 00:41:43 +0000

    Tue, Feb 28, 2023, 1:41 AM +01:00

--- set3/test.py
+++ set3/test.py
@@ -2,7 +2,7 @@ grocery = {}
 while True:
     try:
         item = input("item ")
-        if item in grocery::
+        if item in grocery:
             grocery[item] += 1
         else:
             grocery[item] = 1
