bd75a4fb70157dc36527070e828097df47b97bc5
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-28 00:05:19 +0000

    Tue, Feb 28, 2023, 1:05 AM +01:00

--- set3/test.py
+++ set3/test.py
@@ -1,12 +1,2 @@
-def main():
-    try:
-        x = int(input("type x "))
-        y = int(input("type y "))
-        if x > y:
-            print("yes")
-    except(EOFError):
-        pass
-    
-main()
-
-
+d = dict(input("here "))
+print(d)
