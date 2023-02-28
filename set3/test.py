42d0630c348c90db599fc8bad045880ee26d5bb1
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-02-28 00:28:54 +0000

    Tue, Feb 28, 2023, 1:28 AM +01:00

--- set3/test.py
+++ set3/test.py
@@ -1,8 +1,10 @@
-k = input("key ")
-v = input("value ")
+k = 0
+v = 0
 dict = {k:v}
 while True:
     try:
+        k = input("key ")
+        v = input("value ")
         dict = dict, {k:v}
         print(dict)
     except:
