d642030e7c0db7b11a0b56e0a3bec8fb274ea3f8
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-01 22:05:49 +0000

    Wed, Mar 1, 2023, 11:05 PM +01:00

--- set3/outdated.py
+++ set3/outdated.py
@@ -13,7 +13,7 @@ months =[
     "December"
     ]
 while True:
-    date = input("Date: ")
+    date = input("Date: ").strip()
     try:
         month, day, year = date.split("/")
         if (int(month) >= 1 and int(month) <= 12) and (int(day) >=1 and int(day) <= 31):
