b54f9ddb60df6f2236aa0541910f9d0a83178001
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-01 17:26:31 +0000

    Wed, Mar 1, 2023, 6:26 PM +01:00

--- set3/outdated.py
+++ set3/outdated.py
@@ -13,11 +13,10 @@ monts =[
     "December"
     ]
 while True:
+    date = input("Date: ")
     try:
-        date = input("Date: ")
         month, day, year = date.split("/")
         if (int(month) >= 1 and int(month) <= 12) and (int(day) >=1 and int(day) <= 31):
             print(date)
     except EOFError:
-        print(date)
         break
