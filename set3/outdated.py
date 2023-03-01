483673567521e0176b7df9cc8706467af08b1b8b
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-01 18:13:07 +0000

    Wed, Mar 1, 2023, 7:13 PM +01:00

--- set3/outdated.py
+++ set3/outdated.py
@@ -25,8 +25,7 @@ while True:
                 if old_month == months[i]:
                     month = i + 1
             day = old_day.replace(",", "")
-                else:
-                    
+
             if (int(month) >= 1 and int(month) <= 12) and (int(day) >=1 and int(day) <= 31):
                 break
         except:
