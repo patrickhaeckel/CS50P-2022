ee532334d99c611046ec6d0876304a5f3711373e
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-01 22:03:35 +0000

    Wed, Mar 1, 2023, 11:03 PM +01:00

--- set3/outdated.py
+++ set3/outdated.py
@@ -31,7 +31,6 @@ while True:
             if (int(month) >= 1 and int(month) <= 12) and (int(day) >=1 and int(day) <= 31):
                 break
         except:
-            print()
             pass
 
 print(f"{year}-{int(month):02}-{int(day):02}")
