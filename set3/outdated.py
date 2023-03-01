d488a6e2ba3cb73b3dcb74be0bc55bcdce7111f0
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-01 17:10:53 +0000

    Wed, Mar 1, 2023, 6:10 PM +01:00

---
+++ set3/outdated.py
@@ -0,0 +1,16 @@
+dict =[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+    ]
+while True:
+    
