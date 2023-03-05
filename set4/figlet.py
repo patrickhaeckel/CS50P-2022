5dbd3cbf56e8bfa7c501a5b1869389f38a2535ae
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-05 21:23:20 +0000

    Sun, Mar 5, 2023, 10:23 PM +01:00

--- set4/figlet.py
+++ set4/figlet.py
@@ -3,5 +3,5 @@ import sys
 font = Figlet(font="doh")
 text = input("Text here ")
 
-if len(sys.argv) >2 or len(sys.argv) = 0
+if len(sys.argv) > 2 or len(sys.argv) = 0
 print(font.renderText(f"{text}"))
