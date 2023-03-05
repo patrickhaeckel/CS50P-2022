06eb1ed6e1ce25f7ab0bb8df6f725eed45b3acce
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-05 22:51:23 +0000

    Sun, Mar 5, 2023, 11:51 PM +01:00

--- set4/figlet.py
+++ set4/figlet.py
@@ -1,8 +1,6 @@
 from pyfiglet import Figlet
 from random import shuffle
 import sys
-fonts = Figlet.getFonts(figlet.org/examples.html.)
-print()
 text = input("Text here ")
 if len(sys.argv) == 2:
     font = Figlet(font="doh")
