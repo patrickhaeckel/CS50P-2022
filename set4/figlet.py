3c57f938f97c499bf95fd9b2c17a0bdbd9f67848
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-05 23:10:15 +0000

    Mon, Mar 6, 2023, 12:10 AM +01:00

--- set4/figlet.py
+++ set4/figlet.py
@@ -8,7 +8,8 @@ if len(sys.argv) == 2:
     font = Figlet(font="doh")
     print(font.renderText(f"{text}"))
 elif len(sys.argv) == 1:
-    font = shuffle(fonts)
+    shuffle(fonts)
+    font = fonts[1]
     print(font.renderText(f"{text}"))
 
 
