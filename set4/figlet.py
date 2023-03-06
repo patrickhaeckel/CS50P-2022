e7f100ab4523e59fda6db7b7eedf329dca48cbc9
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-06 23:09:45 +0000

    Tue, Mar 7, 2023, 12:09 AM +01:00

--- set4/figlet.py
+++ set4/figlet.py
@@ -2,7 +2,7 @@ from pyfiglet import Figlet
 from random import shuffle
 import sys
 text = input("Text here ")
-fonts = Figlet.getFonts()
+fonts = figlet.getFonts()
 if len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font") and sys.argv[2] in fonts:
     font = Figlet(font=sys.argv[2])
     print(font.renderText(f"{text}"))
