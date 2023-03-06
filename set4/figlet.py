a2b3cd42eccee30ecd8f8ed705574bf20db6e6ff
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-06 00:18:50 +0000

    Mon, Mar 6, 2023, 1:18 AM +01:00

--- set4/figlet.py
+++ set4/figlet.py
@@ -3,15 +3,17 @@ from random import shuffle
 import sys
 text = input("Text here ")
 if len(sys.argv) == 3:
-    font = Figlet(font=sys.argv[2])
-    print(font.renderText(f"{text}"))
-    if (sys.argv[1])
+    if sys.argv[1] == "--f":
+        font = Figlet(font=sys.argv[2])
+        print(font.renderText(f"{text}"))
+    else:
+        sys.exit()
 elif len(sys.argv) == 1:
     fonts = ["slant", "rectangles", "alphabet"]
     shuffle(fonts)
     font = Figlet(fonts[1])
     print(font.renderText(f"{text}"))
 else:
-    sys.exit()
+    _ = "."
 
 
