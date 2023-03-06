6a07d8f6f7e8c8648e2b535959a23caa82a04526
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-06 23:32:03 +0000

    Tue, Mar 7, 2023, 12:32 AM +01:00

--- set4/figlet.py
+++ set4/figlet.py
@@ -2,16 +2,15 @@ from pyfiglet import Figlet
 from random import shuffle
 import sys
 figlet = Figlet()
-print(figlet)
+fonts = figlet.getFonts()
 if len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font") and sys.argv[2] in fonts:
-    font = Figlet(font=sys.argv[2])
+    figlet.setFont(font=sys.argv[2])
     print(font.renderText(f"{text}"))
 elif len(sys.argv) == 3 and (sys.argv[1] != "-f" or sys.argv[1] != "--font"):
         sys.exit(print("Invalid usage"))
 elif len(sys.argv) == 1:
-    fonts = ["slant", "rectangles", "alphabet"]
     shuffle(fonts)
-    font = Figlet(fonts[1])
+    figlet.setFont(font=sys.argv[1])
     print(font.renderText(f"{text}"))
 else:
      sys.exit(print("Invalid usage"))
