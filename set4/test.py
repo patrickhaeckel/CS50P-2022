c812d3843525c3d9a43d10d3e873b516b5b4794f
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-06 23:04:35 +0000

    Tue, Mar 7, 2023, 12:04 AM +01:00

--- set4/test.py
+++ set4/test.py
@@ -4,19 +4,13 @@ import random
 figlet = Figlet()
 
 # list of fonts
-
 fonts = figlet.getFonts()
-
 if len(sys.argv) == 1: figlet.setFont(font=random.choice(fonts))
-
 elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
     figlet.setFont(font=sys.argv[2])
-
 else:
     sys.exit("Invalid usage")
-
 text = input("Input: ")
-
 print(figlet.renderText(text))
 
 # sys.argv [".\figlet.py", "-f", "slant"]
