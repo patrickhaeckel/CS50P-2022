9989d658b18097d5e95ab283b59e16e640f553e4
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-06 23:49:19 +0000

    Tue, Mar 7, 2023, 12:49 AM +01:00

--- set4/figlet.py
+++ set4/figlet.py
@@ -5,14 +5,11 @@ figlet = Figlet()
 fonts = figlet.getFonts()
 if len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font") and sys.argv[2] in fonts:
     figlet.setFont(font=sys.argv[2])
-    text = input("here ")
-    print(figlet.renderText(f"{text}"))
-
 elif len(sys.argv) == 1:
     shuffle(fonts)
     figlet.setFont(font=(fonts[1]))
-    text = input("here ")
-    print(figlet.renderText(f"{text}"))
+
 else:
      sys.exit("Invalid usage")
-
+text = input("here ")
+print(figlet.renderText(f"{text}"))
