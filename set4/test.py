78551a528ae6152d8803e9d7e9405e2c92eb2a01
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-06 23:05:07 +0000

    Tue, Mar 7, 2023, 12:05 AM +01:00

--- set4/test.py
+++ set4/test.py
@@ -5,7 +5,8 @@ figlet = Figlet()
 
 # list of fonts
 fonts = figlet.getFonts()
-if len(sys.argv) == 1: figlet.setFont(font=random.choice(fonts))
+if len(sys.argv) == 1:
+    figlet.setFont(font=random.choice(fonts))
 elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
     figlet.setFont(font=sys.argv[2])
 else:
