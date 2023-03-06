e81fa1be16ad1fb312d8c49123fea1fed5107365
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-06 17:14:00 +0000

    Mon, Mar 6, 2023, 6:14 PM +01:00

--- set4/test.py
+++ set4/test.py
@@ -6,11 +6,7 @@ if len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font":):
     font = Figlet(font=sys.argv[2])
     print(font.renderText(f"{text}"))
 elif len(sys.argv) == 3 and (sys.argv[1] != "-f" or sys.argv[1] != "--font":):
-    try:
-        sys.exit(print("Invalid usage"))
-    except:
-        print:("Invalid usage")
-        
+    sys.exit(print("Invalid usage"))
 
 elif len(sys.argv) == 1:
     fonts = ["slant", "rectangles", "alphabet"]
