f9bc1962a070daafe55bd72e96a9229fa0e2ef8e
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-06 17:07:55 +0000

    Mon, Mar 6, 2023, 6:07 PM +01:00

--- set4/test.py
+++ set4/test.py
@@ -2,20 +2,19 @@ from pyfiglet import Figlet
 from random import shuffle
 import sys
 text = input("Text here ")
-while True
-    if len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font":):
-        font = Figlet(font=sys.argv[2])
-        print(font.renderText(f"{text}"))
-    elif len(sys.argv) == 3 and (sys.argv[1] != "-f" or sys.argv[1] != "--font":):
-        sys.exit(print("Invalid usage"))
+if len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font":):
+    font = Figlet(font=sys.argv[2])
+    print(font.renderText(f"{text}"))
+elif len(sys.argv) == 3 and (sys.argv[1] != "-f" or sys.argv[1] != "--font":):
+    sys.exit(print("Invalid usage"))
 
-    elif len(sys.argv) == 1:
-        fonts = ["slant", "rectangles", "alphabet"]
-        shuffle(fonts)
-        font = Figlet(fonts[1])
-        print(font.renderText(f"{text}"))
-    else:
-        continue
+elif len(sys.argv) == 1:
+    fonts = ["slant", "rectangles", "alphabet"]
+    shuffle(fonts)
+    font = Figlet(fonts[1])
+    print(font.renderText(f"{text}"))
+else:
+    continue
 
 
 
