54a3d1320d4e5593a1c83983be4a1fed8c831116
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-07 22:30:53 +0000

    Tue, Mar 7, 2023, 11:30 PM +01:00

--- set4/test.py
+++ set4/test.py
@@ -4,12 +4,12 @@ def main():
         try:
             level = int(input("Level: "))
             if level >= 1 and level <= 10:
-                n = random.randint(1, level)
                 otra(level)
         except ValueError:
             main()
 def otra(level):
     if level >= 1 and level <= 10:
+            n = random.randint(1, level)
             guess = int(input("Guess: "))
             if guess > n:
                 print("Too large!")
