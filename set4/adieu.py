83c88730ea8aa0d2e2872d9866d023f132a61537
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-07 15:33:41 +0000

    Tue, Mar 7, 2023, 4:33 PM +01:00

--- set4/adieu.py
+++ set4/adieu.py
@@ -1,5 +1,9 @@
 # implement a program that prompts the user for names, one per line, until the user inputs control-d. Assume that the user will input at least one name. Then bid adieu to those names, separating two names with one and, three names with two commas and one and, and names with commas and one and, as in the below:
-
+dict = {}
 while True:
-    name = input("Names here ")
-    print("")
+    try:
+        name = input("Names here ")
+    except EOFError:
+        for i in dict:
+            print(dict[i])
+
