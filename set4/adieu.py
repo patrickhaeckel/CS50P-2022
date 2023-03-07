6a291119d333efa812e9829bffb1e6c9879440b1
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-07 16:50:33 +0000

    Tue, Mar 7, 2023, 5:50 PM +01:00

--- set4/adieu.py
+++ set4/adieu.py
@@ -1,4 +1,6 @@
 # implement a program that prompts the user for names, one per line, until the user inputs control-d. Assume that the user will input at least one name. Then bid adieu to those names, separating two names with one and, three names with two commas and one and, and names with commas and one and, as in the below:
+import inflect
+p = inflect.engine()
 data = []
 while True:
     try:
@@ -9,7 +11,8 @@ while True:
         break
 print("")
 for n in data:
-    print(n, end="")
+    print(p.join(n))
+    
 
 
 
