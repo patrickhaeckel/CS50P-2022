ea0c9af27ba77c953350bd0f3ef1e6fd7ae424e3
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-07 20:57:55 +0000

    Tue, Mar 7, 2023, 9:57 PM +01:00

--- set4/game.py
+++ set4/game.py
@@ -3,7 +3,9 @@ while True:
     try:
         level = int(input("Level: "))
         guess = int(input("Guess: "))
-        if level >= 1 and level <=10 
+        if level >= 1 and level <=10:
+            n = level.randint()
+            print(n)
 
     except(ValueError):
         print(level, guess)
