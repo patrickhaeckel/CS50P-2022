9c3a0ca47eda376a09c085b62c9a539b98bf19b8
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-07 23:04:50 +0000

    Wed, Mar 8, 2023, 12:04 AM +01:00

--- set4/game.py
+++ set4/game.py
@@ -1,17 +1,18 @@
 import random
 level = int(input("Level: "))
 while True:
-    if level >= 1 and level <= 10:
-        n = random.randint(1, level)
-        guess = int(input("Guess: "))
-        if guess > n:
-            print("Too large!")
-        elif guess < n:
-            print("Too small!")
-        else:
-            print("Just right!")
-            break
-    else:
-        print(level)
+    try:
+        if level >= 1 and level <= 10:
+            n = random.randint(1, level)
+            guess = int(input("Guess: "))
+            if guess > n:
+                print("Too large!")
+            elif guess < n:
+                print("Too small!")
+            else:
+                print("Just right!")
+                break
+    except ValueError:
+        break
 
 
