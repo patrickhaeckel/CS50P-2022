5add6771d69633dadce7c43b532540a44162d196
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-08 20:08:58 +0000

    Wed, Mar 8, 2023, 9:08 PM +01:00

--- set4/professor.py
+++ set4/professor.py
@@ -59,7 +59,7 @@ def sim_game(level):
         response = sim_round(x, y)
         if response == True:
             score +=1
-            counter += 1
+        counter += 1
     return score
 
 if __name__ == "__main__":
