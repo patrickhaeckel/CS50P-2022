b058d9f8aee1cebaa5003551e62f7b662988c867
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-11 22:42:50 +0000

    Sat, Mar 11, 2023, 11:42 PM +01:00

--- set4/bitcoin.py
+++ set4/bitcoin.py
@@ -5,7 +5,8 @@ try:
 
     if sys.argv[1].isdigit():
         index = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        for 
+        
+        for
         print(json.dumps(index.json(), indent=2))
 
 
