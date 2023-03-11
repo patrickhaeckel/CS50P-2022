5c9274c1025e2fb6968d4e745103d341e59e68c3
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-11 22:55:39 +0000

    Sat, Mar 11, 2023, 11:55 PM +01:00

--- set4/bitcoin.py
+++ set4/bitcoin.py
@@ -6,7 +6,7 @@ try:
     if sys.argv[1].isdigit():
         index = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
-        o = index.json()
+        o = index
         for results in o["USD"]:
             print((results["rate_float"])*(sys.argv[1]))
 
