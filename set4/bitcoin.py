65db219645d6b3a97566a28194169492eca93bc7
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-11 22:31:43 +0000

    Sat, Mar 11, 2023, 11:31 PM +01:00

--- set4/bitcoin.py
+++ set4/bitcoin.py
@@ -1,20 +1,9 @@
 import requests
 import sys
-numbtc = (input())
-if numbtc.isdigit():
-    numbtc = float(numbtc)
-    index = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    index.json(numbtc)
-    print(index)
-
-
 try:
-    numbtc = (input())
-    if numbtc.isdigit():
-        numbtc = float(numbtc)
-        index = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        index.json(numbtc)
-        print(index)
+    numbtc =float(input())
+    index = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    print(index.json())
 
 
 
