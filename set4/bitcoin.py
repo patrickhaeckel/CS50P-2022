b1149b08c16d61d3a549acb3ccfada0b8fa9e46f
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-11 22:41:26 +0000

    Sat, Mar 11, 2023, 11:41 PM +01:00

--- set4/bitcoin.py
+++ set4/bitcoin.py
@@ -5,11 +5,12 @@ try:
 
     if sys.argv[1].isdigit():
         index = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        for 
         print(json.dumps(index.json(), indent=2))
 
 
 
 
     ...
-except requests.RequestException:
+except (requests.RequestException, IndexError, ValueError):
     pass
