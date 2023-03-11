0edfdf0d1fda9965b368c2debb42afb3f80c9d06
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-11 23:04:19 +0000

    Sun, Mar 12, 2023, 12:04 AM +01:00

--- set4/bitcoin.py
+++ set4/bitcoin.py
@@ -7,8 +7,8 @@ try:
         index = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 
         o = index.json()
-        for results in o["time"]:
-            print((results["rate_float"])*(sys.argv[1]))
+        for result in o["time"]:
+            print((result["rate_float"])*(sys.argv[1]))
 
     else:
         sys.exit("Missing command-line argument")
