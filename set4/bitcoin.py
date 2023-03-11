33d6931ffd1d7a37eb2a90e1d43debabefc9f9ac
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-11 22:36:04 +0000

    Sat, Mar 11, 2023, 11:36 PM +01:00

--- set4/bitcoin.py
+++ set4/bitcoin.py
@@ -1,7 +1,8 @@
 import requests
+import json
 import sys
 try:
-    numbtc =float(input())
+    numbtc =(input())
     index = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     print(json.dumps(index.json(), indent=2))
 
