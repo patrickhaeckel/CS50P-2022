eb9a569fe851b4d92168c935d89b7c958ef25ab9
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-11 22:46:26 +0000

    Sat, Mar 11, 2023, 11:46 PM +01:00

--- set4/bitcoin.py
+++ set4/bitcoin.py
@@ -5,13 +5,17 @@ try:
 
     if sys.argv[1].isdigit():
         index = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        
-        for
+
+        o = index.json()
+        for algo in o[]
         print(json.dumps(index.json(), indent=2))
+
+    else:
+        sys.exit("Missing command-line argument")
 
 
 
 
     ...
-except (requests.RequestException, IndexError, ValueError):
+except (requests.RequestException, IndexError,):
     pass
