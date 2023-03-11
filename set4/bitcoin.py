304e1a17cf51d975b4882fc69955805fd8a2b342
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-11 23:17:35 +0000

    Sun, Mar 12, 2023, 12:17 AM +01:00

--- set4/bitcoin.py
+++ set4/bitcoin.py
@@ -8,7 +8,7 @@ try:
 
         o = lista.json()
         for result in o["time"]:
-            print(result[0:500])
+            print(result[0:5])
 
     else:
         sys.exit("Missing command-line argument")
