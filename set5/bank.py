a474895aa73f8ac06d50098904204083114f133d
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-13 21:59:57 +0000

    Mon, Mar 13, 2023, 10:59 PM +01:00

---
+++ set5/bank.py
@@ -0,0 +1,17 @@
+def main():
+    word = input("say hello ").replace(" ", "").casefold()
+
+
+def value(greeting):
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+if word.startswith("hello"):
+    print("$0")
+elif word.startswith("h"):
+    print("$20")
