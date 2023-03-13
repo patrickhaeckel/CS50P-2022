d12c710b9f38c20f94b8828c258e66433cdff2a6
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-13 21:21:02 +0000

    Mon, Mar 13, 2023, 10:21 PM +01:00

--- set5/test_twttr.py
+++ set5/test_twttr.py
@@ -1,14 +1,10 @@
 from twttr import shorten
-def main()
-test_numb()
-test_str()
-test_lower()
-test_upper()
-test_both()
+def main():
+    test_str()
+    test_lower()
+    test_upper()
+    test_both()
 
-
-def test_numb():
-    assert shorten(4) == 4
 
 def test_str():
     assert shorten("4") == "4"
