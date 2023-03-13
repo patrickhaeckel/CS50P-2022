63ca47db9f903b360b6b9ffb6c952227b2238e2a
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-13 23:03:40 +0000

    Tue, Mar 14, 2023, 12:03 AM +01:00

--- set5/test_bank.py
+++ set5/test_bank.py
@@ -7,6 +7,7 @@ def test_casefold():
     assert value('HELLO') == ('$0')
     assert value('hola') == ('$20')
     assert value('111') == ('$100')
+    assert value('$100') == ('$100')
 
 
 if __name__ == "__main__":
