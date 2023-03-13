d99245819acb9da6047e3e06477486b8b75606cf
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-13 22:34:35 +0000

    Mon, Mar 13, 2023, 11:34 PM +01:00

--- set5/test_bank.py
+++ set5/test_bank.py
@@ -4,7 +4,7 @@ def main():
 
 
 def test_values():
-    assert value('000') == (None)
+    assert value('HELLO') == (None)
     assert value('HELLO') == ('$0')
     assert value('hOLA MARCO') == ('$20')
 
