677aae9b3a1fec5a202df51de9ab0c8dbecd8e1e
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-13 21:51:32 +0000

    Mon, Mar 13, 2023, 10:51 PM +01:00

--- set5/test_twttr.py
+++ set5/test_twttr.py
@@ -4,7 +4,7 @@ def main():
     test_lower()
     test_upper()
     test_both()
-
+    test_punct()
 
 def test_str():
     assert shorten('4') == '4'
@@ -17,6 +17,9 @@ def test_upper():
 
 def test_both():
     assert shorten('TwItTeR') == 'TwtTR'
+
+def test_punct():
+    assert shorten('?.!') == '?.!'
 
 if __name__ == "__main__":
     main()
