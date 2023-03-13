6caefe8c498c6157ca7b51a196401479c4d0a12a
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-13 23:11:28 +0000

    Tue, Mar 14, 2023, 12:11 AM +01:00

--- set5/bank.py
+++ set5/bank.py
@@ -1,15 +1,16 @@
 def main():
     greeting = input("say hello ").replace(" ", "").lower()
-    print(value(greeting))
+    toprint = value(greeting)
+    print(f"${toprint}")
 
 
 def value(greeting):
     if greeting.startswith("hello"):
-        return(f"${0}")
+        return(0)
     elif greeting.startswith("h"):
-        return(f"${20}")
+        return(20)
     else:
-        return(f"${100}")
+        return(100)
 
 
 if __name__ == "__main__":
