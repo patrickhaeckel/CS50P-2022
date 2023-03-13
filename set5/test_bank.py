cd2883ecf32a24d5b2abf1138326f22f585f0ec5
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-13 22:28:56 +0000

    Mon, Mar 13, 2023, 11:28 PM +01:00

--- set5/test_bank.py
+++ set5/test_bank.py
@@ -5,5 +5,5 @@ def main():
 def test_values():
     assert value('000') == ('000')
     assert value('HELLO') == ('$0')
-    assert value('hi') == ('$20')
+    assert value('OLA MARCO') == ('$20')
     assert value() ==
