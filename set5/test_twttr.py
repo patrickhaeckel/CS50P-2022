d12f17ff479c6ea6503a8f20bb958045fdde78df
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-13 21:18:00 +0000

    Mon, Mar 13, 2023, 10:18 PM +01:00

---
+++ set5/test_twttr.py
@@ -0,0 +1,25 @@
+from twttr import shorten
+def main()
+test_numb()
+test_str()
+test_lower()
+test_upper()
+test_both()
+
+
+def test_numb():
+    assert shorten(4) == 4
+
+def test_str():
+    assert shorten("4") == "4"
+
+def test_lower():
+    assert shorten("vvv") == "vvv"
+
+def test_upper():
+    assert shorten("VVV") == "vvv"
+
+def test_both():
+    assert shorten("vVv") == "vVv"
+
+main()
--- tests/test_twttr.py
+++
@@ -1,6 +0,0 @@
-from twttr import shorten
-def test_numb():
-    assert shorten (4) == 4
-
-def test_str():
-    assert shorten ("4") == "4"
