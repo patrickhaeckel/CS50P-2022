cb67a9d81a8cfdd148b0a9feee7be8af06670cdd
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-14 21:22:31 +0000

    Tue, Mar 14, 2023, 10:22 PM +01:00

--- set2/plates.py
+++ set2/plates.py
@@ -7,17 +7,16 @@ def main():
 
 
 def is_valid(s):
-    if s[0].isalpha() == False or s[1].isalpha() == False:
-        return False
     if len(s) < 2 or len(s) > 6:
         return False
-
+    if s[0].isalpha() == False or s[1].isalpha() == False:
+        return False
     if s[-1].isalpha() == True:
         return False
-
     s = []
+
     for i in s:
-        if s[i] in [".", ",", " "]:
+        for s[i] in [".", ",", " "]:
             return False
         if s[i].isalpha() == False:
             if s[i] == "0":
