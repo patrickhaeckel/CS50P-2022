d8bb571a18ef974fb916bf7c251643fa9aa8c48f
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-14 20:11:25 +0000

    Tue, Mar 14, 2023, 9:11 PM +01:00

--- set2/plates.py
+++ set2/plates.py
@@ -19,9 +19,7 @@ def is_valid(s):
 # For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
 # The first number used cannot be a ‘0’
     i = 0
-    x = len(s)
-    x = str(x)
-    if i[x].isalpha() == True:
+    if str(i[s-1]).isalpha() == True:
         return False
     while (i) < len(s):
         if s[i].isalpha() == False:
