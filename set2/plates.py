7b28c0d05d8f4524f456b4d4d4814181e359053c
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-14 20:31:21 +0000

    Tue, Mar 14, 2023, 9:31 PM +01:00

--- set2/plates.py
+++ set2/plates.py
@@ -14,14 +14,14 @@ def is_valid(s):
 # vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters
     if len(s) < 2 or len(s) > 6:
         return False
+    if s[len(s)].isalpha() == True:
+        return False
 
 # Numbers cannot be used in the middle of a plate; they must come at the end.
 # For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
 # The first number used cannot be a ‘0’
     i = 0
     while (i) < len(s):
-        if s[i-1].isalpha() == True:
-            return False
         if s[i].isalpha() == False:
             if s[i] == "0":
                 return False
