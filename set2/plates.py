68efe43abd52934edf7651e4fd8339207d79074e
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-14 19:50:32 +0000

    Tue, Mar 14, 2023, 8:50 PM +01:00

--- set2/plates.py
+++ set2/plates.py
@@ -34,7 +34,7 @@ def is_valid(s):
 
 # No periods, spaces, or punctuation marks are allowed
 # noper = [".", " ", "!"]
-    if s[i] == [".", " ", "!"]:
+    if s == [".", " ", "!"]:
         return False
     return True
 main()
