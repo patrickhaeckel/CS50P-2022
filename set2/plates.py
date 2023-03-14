754b15358a4bcf1297cee2b6c98132138ce7f3cf
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-14 20:46:21 +0000

    Tue, Mar 14, 2023, 9:46 PM +01:00

--- set2/plates.py
+++ set2/plates.py
@@ -29,7 +29,7 @@ def is_valid(s):
                 return False
             elif s[i+1].isalpha() == True:
                 return False
-            if s[i] == [".", " ", "!"]:
+            if s[i] == ["."]:
                 return False
             else:
                 break
