0f104679db6bf82bcdc671314e2dffe0b49349b6
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-16 19:14:37 +0000

    Thu, Mar 16, 2023, 8:14 PM +01:00

--- set2/test.py
+++ set2/test.py
@@ -1,8 +1,8 @@
 x = input("aqui ")
 i = 0
-while i <= len (x):
-        if x[0].isdigit():
-            print("es el ultimo")
+while i < len (x):
+        if x[i].isdigit():
+            print("!!!!!!!")
 
         i += 1
         print(i)
