2a577109e8b9e0e53b40beff5d10bd8f7453fe48
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-23 23:11:01 +0000

    Fri, Mar 24, 2023, 12:11 AM +01:00

--- set6/pizza/pizza.py
+++ set6/pizza/pizza.py
@@ -7,7 +7,7 @@ while True:
         if len(sys.argv) == 2:
             with open(sys.argv[1], "r") as file:
                 table = csv.reader(file)
-                for row in reader:
+                for row in table:
                     menu.append(row)
             print(menu)
     except:
