e766602d985d6fa487649c942b381911f6e39e83
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-23 23:10:20 +0000

    Fri, Mar 24, 2023, 12:10 AM +01:00

--- set6/pizza/pizza.py
+++ set6/pizza/pizza.py
@@ -1,9 +1,14 @@
 import sys
 import csv
 
-while True
+menu = []
+while True:
     try:
         if len(sys.argv) == 2:
             with open(sys.argv[1], "r") as file:
-                    table = csv.reader(file)
-                    for row in reader
+                table = csv.reader(file)
+                for row in reader:
+                    menu.append(row)
+            print(menu)
+    except:
+        raise
