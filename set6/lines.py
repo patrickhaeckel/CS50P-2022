443fa881d01e6467391159d0b8e3ddc1ed1912b4
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-23 19:55:08 +0000

    Thu, Mar 23, 2023, 8:55 PM +01:00

--- set6/lines.py
+++ set6/lines.py
@@ -1,21 +1,22 @@
 import sys
 
-n = 0
-if sys.argv[1].endswith(".py"):
-    print("yes")
-    if len(sys.argv) == 2:
+while True:
+    n = 0
+    if sys.argv[1].endswith(".py"):
+        print("yes")
+        if len(sys.argv) == 2:
 
-        with open("linestxt.py", "r") as file:
-            lines = file.readlines()
-        for line in lines:
-            if line[0] == "#" or line == "\n":
-                n = n
-            else:
-                n += 1
-
-        print(n)
-else:
-    sys.exit("se slaio")
+            with open("linestxt.py", "r") as file:
+                lines = file.readlines()
+            for line in lines:
+                if line[0] == "#" or line == "\n":
+                    n = n
+                else:
+                    n += 1
+            print(n)
+            break
+    else:
+        sys.exit()
 
 
 
