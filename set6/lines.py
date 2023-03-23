c71e3b4132c2dff5c02755bd3d577558a1de5132
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-23 19:11:26 +0000

    Thu, Mar 23, 2023, 8:11 PM +01:00

--- set6/lines.py
+++ set6/lines.py
@@ -1,6 +1,7 @@
 import sys
 n = 0
-if len(sys.argv) == 2 and sys.argv[1] == :
+if len(sys.argv) == 2:
+    
     with open("linestxt.py", "r") as file:
         lines = file.readlines()
     for line in lines:
