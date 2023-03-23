af5fbca7ee45520f7cdf991c278b24fc48863d00
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-23 20:06:51 +0000

    Thu, Mar 23, 2023, 9:06 PM +01:00

--- set6/lines/lines.py
+++ set6/lines/lines.py
@@ -1,24 +1,27 @@
 import sys
 
 while True:
-    n = 0
-    if sys.argv[1].endswith(".py"):
-        if len(sys.argv) == 2:
+    try:
+        n = 0
+        if sys.argv[1].endswith(".py"):
+            if len(sys.argv) == 2:
 
-            with open("sys.argv[1]", "r") as file:
-                lines = file.readlines()
-            for line in lines:
-                if line[0] == "#" or line == "\n":
-                    n = n
-                else:
-                    n += 1
-            print(n)
-            break
+                with open(sys.argv[1], "r") as file:
+                    lines = file.readlines()
+                for line in lines:
+                    if line[0] == "#" or line == "\n":
+                        n = n
+                    else:
+                        n += 1
+                print(n)
+                break
 
+            else:
+                sys.exit("Too many command-line arguments")
         else:
-            sys.exit("Too many command-line arguments")
-    else:
-        sys.exit("Not a python file")
+            sys.exit("Not a python file")
+    except:
+        raise
 
 
 
