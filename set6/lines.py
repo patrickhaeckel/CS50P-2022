6ed87ed1b724dc1def9399ec31da094e752b3fa3
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-23 18:59:45 +0000

    Thu, Mar 23, 2023, 7:59 PM +01:00

--- set6/lines.py
+++ set6/lines.py
@@ -4,8 +4,9 @@ n = 0
 with open("linestxt.py", "r") as file:
     lines = file.readlines()
 for line in lines:
-    if line[0] == "#":
-        break
-    n += 1
+    if line[0] == "#" or line == (""):
+        n = n
+    else:
+        n += 1
 print(n)
 
