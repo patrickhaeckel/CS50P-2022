adf079c6dde46bd8a0e8ab79be0d6e9b1656e7f8
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-23 19:07:02 +0000

    Thu, Mar 23, 2023, 8:07 PM +01:00

--- set6/lines.py
+++ set6/lines.py
@@ -1,13 +1,10 @@
 import sys
-
 n = 0
 with open("linestxt.py", "r") as file:
     lines = file.readlines()
 for line in lines:
-    if line[0] == "#":
-        print("hay una")
-    elif line[0] == "":
-        print("hay una")
+    if line[0] == "#" or line == "\n":
+        n = n
     else:
         n += 1
 print(n)
