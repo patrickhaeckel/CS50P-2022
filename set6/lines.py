eabe95eca319fb3e77ef2d190404f232cd9fef88
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-23 19:26:01 +0000

    Thu, Mar 23, 2023, 8:26 PM +01:00

--- set6/lines.py
+++ set6/lines.py
@@ -2,18 +2,17 @@ import sys
 n = 0
 while True:
     try:
-        
         if len(sys.argv) == 2:
+            x = (sys.argv)
+            if ".py" is x:
+                print("yes")
 
-
-
-
-    with open("linestxt.py", "r") as file:
-        lines = file.readlines()
-    for line in lines:
-        if line[0] == "#" or line == "\n":
-            n = n
-        else:
-            n += 1
+        with open("linestxt.py", "r") as file:
+            lines = file.readlines()
+        for line in lines:
+            if line[0] == "#" or line == "\n":
+                n = n
+            else:
+                n += 1
 print(n)
 
