28155ad527a4659cfb38df3d19ed53eb708ae1fd
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-23 23:46:47 +0000

    Fri, Mar 24, 2023, 12:46 AM +01:00

--- set6/pizza/pizza.py
+++ set6/pizza/pizza.py
@@ -11,16 +11,15 @@ while True:
             sys.exit("Too few command-line arguments")
 
         elif len(sys.argv) == 2:
-            if sys.argv[1].endswith(".py"):
+            if sys.argv[1].endswith(".csv"):
                 with open(sys.argv[1], "r") as file:
                     table = csv.reader(file)
                     for row in table:
                         menu.append(row)
             else:
                 sys.exit("Not a Python file")
-
+            print(tabulate(menu[1:], headers=menu[0], tablefmt="grid"))
             break
 
     except FileNotFoundError:
             sys.exit("File does not exist")
-print(tabulate(menu[1:], headers=menu[0], tablefmt="grid"))
