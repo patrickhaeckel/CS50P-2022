8d23cdfe24f23b1a51861ded94d41fc5a51ebae5
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-24 15:19:15 +0000

    Fri, Mar 24, 2023, 4:19 PM +01:00

--- set6/scourgify/scourgify.py
+++ set6/scourgify/scourgify.py
@@ -1,7 +1,13 @@
 import sys
 import csv
+
+info = {}
 if len(sys.argv) == 3:
     if sys.argv[1].endswith(".csv"):
         with open(sys.argv[1], "r") as file:
-            
+            students = dict.reader(file)
+            for row in students:
+                info.append(row)
+    print(info)
+
 
