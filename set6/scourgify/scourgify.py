39dfb710310fb306b889dae72e9f3a46f1281a71
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-24 21:39:25 +0000

    Fri, Mar 24, 2023, 10:39 PM +01:00

--- set6/scourgify/scourgify.py
+++ set6/scourgify/scourgify.py
@@ -5,7 +5,9 @@ if len(sys.argv) == 3:
     if sys.argv[1].endswith(".csv") and sys.argv[2].endswith("csv"):
         with open(sys.argv[1], "r") as file:
             reader = csv.DictReader(file)
-          
+            for row in reader:
+                split_name = 
+
 
 
 
