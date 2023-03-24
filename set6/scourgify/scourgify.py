ddd7a8150dd6bb82b9e15440e6b36c88abc4a9d1
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-24 15:28:15 +0000

    Fri, Mar 24, 2023, 4:28 PM +01:00

--- set6/scourgify/scourgify.py
+++ set6/scourgify/scourgify.py
@@ -1,14 +1,13 @@
 import sys
 import csv
-from tabulate import tabulate
 
-info = {}
+information = []
 if len(sys.argv) == 3:
     if sys.argv[1].endswith(".csv"):
         with open(sys.argv[1], "r") as file:
             students = csv.DictReader(file)
             for row in students:
-                info.append
-    print(info)
+                information.append(row)
+    print(information)
 
 
