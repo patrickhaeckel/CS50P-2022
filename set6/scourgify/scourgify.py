ec7c00e9b94d1ab78ce7d41f8d3a1a9e013e4b9e
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-24 21:43:06 +0000

    Fri, Mar 24, 2023, 10:43 PM +01:00

--- set6/scourgify/scourgify.py
+++ set6/scourgify/scourgify.py
@@ -1,12 +1,15 @@
 import sys
 import csv
 
+output = []
 if len(sys.argv) == 3:
     if sys.argv[1].endswith(".csv") and sys.argv[2].endswith("csv"):
         with open(sys.argv[1], "r") as file:
             reader = csv.DictReader(file)
             for row in reader:
-                split_name = 
+                split_name = row["name"].split(",")
+                output.append({"first"})
+
 
 
 
