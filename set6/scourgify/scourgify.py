61019a3627a079b1c1c4bd6479451b134cac12ee
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-24 20:49:42 +0000

    Fri, Mar 24, 2023, 9:49 PM +01:00

--- set6/scourgify/scourgify.py
+++ set6/scourgify/scourgify.py
@@ -1,16 +1,13 @@
 import sys
 import csv
 
-info = []
 if len(sys.argv) == 3:
     if sys.argv[1].endswith(".csv"):
         with open(sys.argv[1], "r") as file:
             students = csv.DictReader(file)
-            for row in students:
-                info.append(row)
 
-        with open(sys.argv[2], "a") as file2:
-            writer = csv.DictWriter(file2, fieldnames=["name", "home", ])
+        with open(sys.argv[2], "w") as file2:
+            writer = csv.DictWriter(file2, fieldnames=["first", "last", "house"])
 
 
 
