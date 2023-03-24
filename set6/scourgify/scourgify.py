0dd6fe57ae21b225661c4c5e76d295ab57ff2ebd
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-24 21:56:22 +0000

    Fri, Mar 24, 2023, 10:56 PM +01:00

--- set6/scourgify/scourgify.py
+++ set6/scourgify/scourgify.py
@@ -3,7 +3,7 @@ import csv
 
 output = []
 if len(sys.argv) == 3:
-    if sys.argv[1].endswith(".csv") and sys.argv[2].endswith("csv"):
+    if sys.argv[1].endswith("csv") and sys.argv[2].endswith("csv"):
         with open(sys.argv[1], "r") as file:
             reader = csv.DictReader(file)
             for row in reader:
