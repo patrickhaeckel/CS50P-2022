2cb279a2e5dd67f0d3f88b181e0aa2b2077261ec
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-24 20:50:27 +0000

    Fri, Mar 24, 2023, 9:50 PM +01:00

--- set6/scourgify/scourgify.py
+++ set6/scourgify/scourgify.py
@@ -5,9 +5,8 @@ if len(sys.argv) == 3:
     if sys.argv[1].endswith(".csv"):
         with open(sys.argv[1], "r") as file:
             students = csv.DictReader(file)
-
-        with open(sys.argv[2], "w") as file2:
-            writer = csv.DictWriter(file2, fieldnames=["first", "last", "house"])
+            with open(sys.argv[2], "w") as file2:
+                writer = csv.DictWriter(file2, fieldnames=["first", "last", "house"])
 
 
 
