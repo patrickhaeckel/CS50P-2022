25771ddacd1a6582f13accc25242f64331998d1e
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-27 19:55:51 +0000

    Mon, Mar 27, 2023, 9:55 PM +02:00

--- set7/numb3rs/numb3rs.py
+++ set7/numb3rs/numb3rs.py
@@ -1,13 +1,12 @@
 import re
 import sys
 
-
 def main():
     print(validate(input("IPv4 Address: ")))
 
 
 def validate(ip):
-    if re.search(r"[0-9].+\.[0-9].+", ip):
+    if re.search("[0-255]+\.[0-75]+", ip):
         print(ip)
     else:
         print("false")
