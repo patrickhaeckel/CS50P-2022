d20b2f8aefc0521b0da1d1a15f3dc33b445da0ea
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-28 13:20:33 +0000

    Tue, Mar 28, 2023, 3:20 PM +02:00

--- set7/numb3rs/numb3rs.py
+++ set7/numb3rs/numb3rs.py
@@ -6,7 +6,7 @@ def main():
 
 
 def validate(ip):
-    if re.search("[0-255]+\.+[0-75]", ip):
+    if re.search("[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.", ip):
         print("valid")
     else:
         print("invalid")
