1261be025465ee634405f9814529efbeafaf8422
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-28 17:19:01 +0000

    Tue, Mar 28, 2023, 7:19 PM +02:00

--- set7/numb3rs/test_numb3rs.py
+++ set7/numb3rs/test_numb3rs.py
@@ -1,9 +1,9 @@
 from numb3rs import validate
 def main():
-    print(test_validate(ip))
+    print(test_validate())
 
 
-def test_validate(ip):
+def test_validate():
     assert validate('255.1.1.1') == (True)
     assert validate('256.1.1.1') == (False)
     assert validate('255.256.1.1') == (False)
