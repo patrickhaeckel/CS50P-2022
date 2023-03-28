d595dd8b8074ac21e710120713fdd64e3edf4436
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-28 15:05:47 +0000

    Tue, Mar 28, 2023, 5:05 PM +02:00

--- set7/numb3rs/numb3rs.py
+++ set7/numb3rs/numb3rs.py
@@ -2,12 +2,14 @@ import re
 import sys
 
 def main():
-    print(validate(input("IPv4 Address: ")))
+    print(validate(input("IPv4 Address: ").strip()))
 
 
 def validate(ip):
     if re.search("[0-9]+\.[0-9]+\.[0-9]+\.[0-9]", ip):
+        n1, n2, n3, n4 = ip.split(".")
         print("valid")
+        print(n1, n2, n3, n4)
     else:
         print("invalid")
 
