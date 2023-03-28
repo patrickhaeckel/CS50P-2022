eaa9ff3f4b8bdf1a3159e59f75cea98ea351e43a
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-28 17:06:38 +0000

    Tue, Mar 28, 2023, 7:06 PM +02:00

--- set7/numb3rs/test_numb3rs.py
+++ set7/numb3rs/test_numb3rs.py
@@ -5,6 +5,6 @@ def main():
 
 def test_validate(ip):
     assert validate('255.1.1.1') == (True)
-    assert validate('hola') == (20)
-    assert validate('111') == (100)
-    assert validate('$100') == (100)
+    assert validate('256.1.1.1') == (False)
+    assert validate('255.256.1.1') == (False)
+    assert validate('255.1.1.256') == (False)
