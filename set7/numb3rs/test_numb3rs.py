6af788bc98e93d11e06db047e811ca52d185959d
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-03-28 16:55:30 +0000

    Tue, Mar 28, 2023, 6:55 PM +02:00

--- set7/numb3rs/test_numb3rs.py
+++ set7/numb3rs/test_numb3rs.py
@@ -1,10 +1,10 @@
 from numb3rs import validate
 def main():
-    print(test_casefold())
+    print(test_validate(ip))
 
 
-def test_validate():
-    assert validate('HELLO') == (0)
+def test_validate(ip):
+    assert validate('255.') == (0)
     assert validate('hola') == (20)
     assert validate('111') == (100)
     assert validate('$100') == (100)
