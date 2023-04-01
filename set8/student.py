7630af03b2a50606a5fb243776227471ae58b97e
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-04-01 22:40:52 +0000

    Sun, Apr 2, 2023, 12:40 AM +02:00

--- set8/student.py
+++ set8/student.py
@@ -1,15 +1,2 @@
-def main():
-    student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
-
-
-def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return {"name": name, "house": house}
-
-
-if __name__ == "__main__":
-    main()
+fib = {1: 1, 2: 1, 3: 2, 4: 3}
+print(fib.get(2))
