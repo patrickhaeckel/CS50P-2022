24f7664f9956cfc88e0f9a9f46485cef865c83de
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-04-04 19:55:52 +0000

    Tue, Apr 4, 2023, 9:55 PM +02:00

--- oop/car/car.py
+++ oop/car/car.py
@@ -8,7 +8,7 @@ class Car:
         self.color = color
 
     def drive(self):
-        print("This car is driving")
+        print(f"This {self.make} is driving")
 
     def stop(self):
         print("This car is stopped")
