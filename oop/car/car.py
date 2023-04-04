bdbb773d6a3a55c8c07b980224bff6862a932a02
Patrick Haeckel <110048579+haeckelfe@users.noreply.github.com>
2023-04-04 19:46:02 +0000

    Tue, Apr 4, 2023, 9:46 PM +02:00

--- oop/car/car.py
+++ oop/car/car.py
@@ -1,6 +1,14 @@
 class Car:
 
-    make = "mazda"
-    model = 2
-    year = 2013
-    color = "black"
+def __init__(self, make, model, year, color):
+
+    self.make = make
+    self.model = model
+    self.year = year
+    self.color = color
+
+def drive(self):
+    print("This car is driving")
+
+def stop(self):
+    print("This car is stopped")
